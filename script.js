36d919923597c27c8f566e96b7b36a62d8502749
Captain Ahab <captainahab337@gmail.com>
2021-03-05 15:36:16 -0700

    Added additional classes to HTML, finished CSS style sheet, added number range data in JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const characterAmountRange = document.getElementById
+('characterAmountRange')
+const characterAmountNumber = document.getElementById
+('characterAmountNumber')
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+function syncCharacterAmount(e) {
+    const value = e.target.value
+    characterAmountNumber.value = value
+    characterAmountRange.value = value
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 
 }
 
 function buttonFunction() {
-  var x = document.getElementById("myDiv");
-  if (x.style.display === "block") {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "block") {
     x.style.display = "none";
-  } else {
+    } else {
     x.style.display = "block";
-  }
+    }
 }
 
 // Add event listener to generate button
