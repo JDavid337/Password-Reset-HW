246a40602f0f806e6207a94f4a832bba2dda8690
Captain Ahab <captainahab337@gmail.com>
2021-03-07 22:09:40 -0700

    updated JavaScript with notation, created README

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Assignment Code
-
+//The following are the variables and arrays created for the elements of the password
 var passwordText = document.querySelector("#password");
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var specialCharacters = ['@','#','$','%','^','&','*'];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 
+//These variables are left blank for now
 var confirmLength = "";
 var password = "";
 var userChoices = "";
 
+//This initiates the prompts, beginning with setting the range of charaters available for the password.
 while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
     confirmLength = prompt("Number of characters of your new password? Please select between 8 and 128.");
     confirmLength = parseInt(confirmLength);
@@ -17,6 +19,7 @@ while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
         break;
 }
 
+//Once the correct number of characters has been selected, criteria follow asking user to choose specific character types.
     if(confirmLength < 8 || confirmLength > 128){
         alert("please select a range of 8 to 128 characters");
     }
@@ -29,19 +32,21 @@ while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
         }
         if(confirm("Include lowercase letters in your new password?") == true) {
             lowerCase = true
-            userChoices = lowerCase + userChoices;
+            userChoices = upperCase + lowerCase + userChoices;
             console.log(true)
         }
         if(confirm("Include Symbols in your new password?") == true) {
             specialCharacters = true
-            userChoices = specialCharacters + userChoices;
+            userChoices = upperCase + lowerCase + specialCharacters + userChoices;
             console.log(true)
         }
         if(confirm("Include Numbers in your new password?") == true) {
             numbers = true
-            userChoices = numbers + userChoices;
+            userChoices = upperCase + lowerCase + specialCharacters + numbers + userChoices;
             console.log(true)
         }
+
+        //If none of the character types are selected, the user is prompted with this alert
         if (!lowerCase && !upperCase && !specialCharacters && !numbers) {
             alert("you must select at least one character type");
         } else {
