5de9e465111e271e386c08d5c811a6c742ea86d9
Captain Ahab <captainahab337@gmail.com>
2021-03-07 19:29:02 -0700

    updated JavaScript for password prompts, modified HTML to remove unnecessary code

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
 var passwordText = document.querySelector("#password");
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var specialCharacters = ['@','#','$','%','^','&','*'];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
 
-const characterAmountRange = document.getElementById
-('characterAmountRange')
-const characterAmountNumber = document.getElementById
-('characterAmountNumber')
+var confirmLength = "";
+var password = "";
+var userChoices = "";
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
-
-function syncCharacterAmount(e) {
-    const value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value
+while (isNaN(confirmLength) || confirmLength < 8 || confirmLength > 128) {
+    confirmLength = prompt("Number of characters of your new password? Please select between 8 and 128.");
+    confirmLength = parseInt(confirmLength);
+    if (confirmLength === true) {
+        break;
 }
+
+    if(confirmLength < 8 || confirmLength > 128){
+        alert("please select a range of 8 to 128 characters");
+    }
+    else {
+        if(confirmLength) {
+        if (confirm("Include Uppercase letters in your new password?") == true) {
+            upperCase = true
+            userChoices = upperCase + userChoices;
+            console.log(true)
+        }
+        if(confirm("Include lowercase letters in your new password?") == true) {
+            lowerCase = true
+            userChoices = lowerCase + userChoices;
+            console.log(true)
+        }
+        if(confirm("Include Symbols in your new password?") == true) {
+            specialCharacters = true
+            userChoices = specialCharacters + userChoices;
+            console.log(true)
+        }
+        if(confirm("Include Numbers in your new password?") == true) {
+            numbers = true
+            userChoices = numbers + userChoices;
+            console.log(true)
+        }
+        if (!lowerCase && !upperCase && !specialCharacters && !numbers) {
+            alert("you must select at least one character type");
+        } else {
+            for (var i = 0; i < confirmLength; i++) {
+                var singleRandomChar = userChoices[Math.floor(Math.random() * userChoices.length)];
+                password += singleRandomChar;
+            }
+        }
+    }
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-
 }
 
-function buttonFunction() {
-    var x = document.getElementById("myDiv");
-    if (x.style.display === "block") {
-    x.style.display = "none";
+    if (!password) {
+    passwordText.value = "Please regenerate a new password."
     } else {
-    x.style.display = "block";
+    passwordText.value = password;
+    }
     }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
